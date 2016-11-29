30df929e2634dde5d3a45d9c1dc6578de05df115
mayukataoka <mayu.kataoka@gmail.com>
2016-11-28 21:41:25 -0800

    Added a data driven test

--- pages/menu-page.js
+++ pages/menu-page.js
@@ -1,17 +1,8 @@
-/**
- * Created by mumpiko on 11/16/16.
- */
-/**
- * Page object for Protractor website menu.
- * @constructor
- */
+
 var MenuPage = function() {
+
     this.dropdown = function(dropdownName) {
-        /**
-         * Dropdown api. Used to click on an element under a dropdown.
-         * @param {string} dropdownName
-         * @return {{option: Function}}
-         */
+
         var openDropdown = function() {
             element(by.css('.navbar-nav'))
                 .element(by.linkText(dropdownName))
@@ -19,11 +10,6 @@ var MenuPage = function() {
         };
 
         return {
-            /**
-             * Get an option element under a dropdown.
-             * @param {string} optionName
-             * @return {ElementFinder}
-             */
             option: function(optionName) {
                 openDropdown();
                 return element(by.css('.dropdown.open'))
--- specs/smoke/api-search-spec.js
+++ specs/smoke/api-search-spec.js
@@ -1,9 +1,9 @@
 'use strict';
 var SearchBoxPage = require('../../pages/api-searchbox-page.js');
 var Menu = require('../../pages/menu-page.js');
+var testData = require('../../test-data.json');
 
-
-describe('API searchbox , ', function() {
+describe('API searchbox , ', function () {
 
     var searchBox = new SearchBoxPage();
     var menu = new Menu();
@@ -13,12 +13,15 @@ describe('API searchbox , ', function() {
         browser.get(browser.baseUrl);
         menu.dropdown('Reference').option('Protractor API').click();
         expect(browser.getCurrentUrl())
-            .toBe('http://www.protractortest.org/#/api');
+            .toBe(browser.baseUrl + '/#/api');
     });
 
-    it('should be able to receive a search term from a user and display a correct result.', function () {
-        var searchTerm = 'filter';
-        searchBox.searchApi(searchTerm);
-        expect(element(by.linkText(searchTerm)).isDisplayed()).toBe(true);
+    testData.forEach(function (testdata) {
+        it('should be able to receive a search term from a user and display a correct result.', function () {
+            var searchTerm = testdata.searchKey;
+            searchBox.searchApi(searchTerm);
+            expect(element(by.linkText(searchTerm)).isDisplayed()).toBe(true);
+        });
     });
+
 });
